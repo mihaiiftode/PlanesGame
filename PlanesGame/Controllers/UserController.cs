5da5b9582fa820a81ef17a17786ef7e5ad1d201e
Vivendus <ifty_miky@yahoo.com>
2015-07-27 13:06:31 +0300

    Update UserController.cs

--- PlanesGame/Controllers/UserController.cs
+++ PlanesGame/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using PlanesGame.Views;
+using System.Drawing;
+using PlanesGame.Views;
 
 namespace PlanesGame.Controllers
 {
@@ -9,6 +10,63 @@ namespace PlanesGame.Controllers
         public UserController(IUserView view)
         {
             _view = view;
+        }
+
+
+        public void StartNewGame()
+        {
+            SetPlayerName();
+            SetKillRules();
+        }
+
+        public void StartServer()
+        {
+            StartNewGame();
+        }
+
+        public void ConnectToGame()
+        {
+            StartNewGame();
+        }
+
+        public void Disconnect()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetOponent(string type)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetKillRules()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetPlayerName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetUp(Point location)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ExecuteAttack(Point location)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetEnginePlayer(Graphics graphicsObject, Rectangle clientRectangle)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetEngineOponent(Graphics graphicsObject, Rectangle clientRectangle)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
